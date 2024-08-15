c955f00a16ee44c29f61df0708e4c2e520f22e35
Christian Reyes <christian7reyes@berkeley.edu>
2024-08-15 11:09:41 -0700

    revamped site to single page

--- script/script.js
+++ script/script.js
@@ -41,6 +41,7 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 
+
 // Add class navbarDark on navbar scroll
 const header = document.querySelector('.navbar');
 
@@ -114,3 +115,20 @@ document.onkeydown = (e) => {
     return false;
 };
 */
+
+
+
+$(document).ready(function() {
+    var banner_height = $("#navscroll").height();
+    var lastScrollTop = 0;
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+        var currScrollTop = $(this).scrollTop();
+        if (scroll >= banner_height && currScrollTop > lastScrollTop) {
+            $("#banner").hide();
+        } else {
+            $("#banner").show();
+        }
+        lastScrollTop = currScrollTop;
+    });
+});
